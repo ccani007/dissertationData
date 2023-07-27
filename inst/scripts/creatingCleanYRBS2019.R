b4b40e1bd609b0688574f8daa31eb7f1575f93b9
Catalina Canizares Escobar <catcaniz@fiu.edu>
2023-07-27 16:13:10 -0400

    cleaning variable names

--- inst/scripts/creatingCleanYRBS2019.R
+++ inst/scripts/creatingCleanYRBS2019.R
@@ -8,9 +8,9 @@
 
 library(tidyverse)
 
-load("inst/extData/raw_yrbs_2019.rda")
+load("inst/extData/raw2019.rda")
 
-raw_yrbss <- tidyREDCap::drop_labels(raw_yrbs_2019)
+raw_yrbss_2019_df <- tidyREDCap::drop_labels(raw2019)
 # 13677x235
 
 ### This function is created to recode binary factors from 1 and 2 to 0 and 1.
@@ -22,28 +22,20 @@ RecodeBinary <- function(x) {
   )
 }
 
-RecodeNoYesNo <- function(x) {
-  x <- case_when(
-    x %in% c(1, 3) ~ 0,
-    x == 2 ~ 1,
-    TRUE ~ NA
-  )
-}
+# The predictors will be the Dichotomous variables from the YRBS.
+#  The dichotomous variables present the percentage of students answering the
+#  predetermined response(s) of interest (ROI). Students answering the ROI(s)
+#  are in the numerator. The denominator is either all students or a subset of
+#  students who have indicated they participate in a selected activity or behavior.
+#  Students must have provided valid data to be included in any dichotomous
+#  variable calculations.
 
-# This function considers the not sure response as a no.
-RecodeYesNoNo <- function(x) {
-  x <- case_when(
-    x %in% c(2, 3) ~ 0,
-    x == 1 ~ 1,
-    TRUE ~ NA
-  )
-}
 
-healthyBehaviors <-
-  raw_yrbss |>
+clean_yrbs_2019_df <-
+  raw_yrbss_2019_df |>
   select(
-    Q1:Q3, "raceeth", Q58:Q89, Q92:Q99
-  ) |>
+    weight, stratum, psu, Q1, Q2, Q3, Q4,
+    Q6, Q7, Q66, raceeth, starts_with("qn")) |>
   mutate(
     Sex = case_when(
       Q2 == 2 ~ "Male",
@@ -94,53 +86,101 @@ healthyBehaviors <-
       TRUE ~ NA_character_
     )
   ) |>
-  mutate(across(c(Q58, Q98), RecodeBinary)) |>
-  mutate(across(Q62:Q63, RecodeNoYesNo)) |>
-  mutate(across(c(Q84, Q85, Q87, Q94), RecodeYesNoNo)) |>
-  mutate(across(c(Q58, Q98, Q62:Q63,Q84, Q82, Q85, Q87, Q94, Q97), as.factor)) |>
-  mutate(across(c(Q65, Q67, Q68, Q89, Q99), as.character)) |>
-  select(-c(Q2, Q1, Q3, Q66, raceeth)) |>
+  mutate(across(c(QN8:QN99), RecodeBinary)) |>
+  select(-c(Q1, Q2, Q3, Q66, raceeth, qn65, qn66 )) |>
+  select(weight, stratum, psu, Sex, Race, Age, Grade,
+         SexOrientation, everything()) |>
   rename(
-    HadSex = Q58,
-    AgeSex = Q59,
-    SexPartners = Q60,
-    SexPartners3Months = Q61,
+    SeatBealtUse = QN8,
+    DrinkingDriver = QN9,
+    DrivingDrinking = QN10,
+    TextingDriving = QN11,
+    WeaponCarrying = QN12,
+    WeaponCarryingSchool = QN13,
+    GunCarrying = QN14,
+    UnsafeAtSchool = QN15,
+    InjuredInSchool = QN16,
+    PhysicalFight = QN17,
+    SchoolPhysicalFight = QN18,
+    ForcedSexualIntercourse = QN19,
+    SexualViolence= Q20,
+    SexualAbuseByPartner = Q21,
+    PhysicalDaitingViolence = QN22,
+    Bullying = Q23,
+    CyberBullying = Q24,
+    Hopelessness = QN25,
+    SuicideIdeation = QN26,
+    SuidicePlan = QN27,
+    SuicideAttempts = QN28,
+    SuicideInjuryMedicalAttention = QN29,
+    EverTriedCigarrette = QN30,
+    FirstCigBefore13 = QN31,
+    CurrentlySmokingCigarette = QN32,
+    MoreThan10CigPerDay = QN33,
+    EverTriedVaping = QN34,
+    CurrentlyVaping = QN35,
+    StoreSourceVaping = QN36,
+    CurrentlySmokelessTobacco = QN37,
+    CurrentlySmokingCigar = QN38,
+    QuitTobbaco = QN39,
+    FirstAlcoholBefore13 = QN40,
+    CurrentlyAlcohol = Q41,
+    CurrentlyBingeDrinking = Q42,
+    MoreThan10Drinks = Q43,
+    SomeoneSourceAlcohol = Q44,
+    EverUsedMarihuana = Q45,
+    FirstMarihuanaBefore13 = Q46,
+    CurrentlyUseMarihuana = Q47,
+    EverUsedSyntheticMarihuana = Q48,
+    PainMedicine = Q49,
+    EverUsedCocaine = Q50,
+    EverUsedInhalant = Q51,
+    EverUsedHeroin = Q52,
+    EverUsedMetha = Q53,
+    EverUsedEcstasy = Q54,
+    EverUsedSteroids = Q55,
+    EverUsedInjectedIllegalDrug = Q56,
+    OfferedDrugsSchool = Q57,
+    EverHadSex = Q58,
+    SexBefore13 = Q59,
+    Sex4OrMorePartners = Q60,
+    SexuallyActive = Q61,
     AlcoholOrDrugsSex = Q62,
     UseCondom = Q63,
     BirthControl = Q64,
-    SexOfSexualPartner = Q65,
-    DescribeWeight = Q67,
+    VeryOverweight = QN67,
     WeightLoss = Q68,
-    DrinkFruitJuice = Q69,
-    EatFruit = Q70,
-    EatSalad = Q71,
-    EatPotatoes = Q72,
-    EatCarrots = Q73,
-    EatOtherVeggies = Q74,
-    DrinkSoda = Q75,
-    DrinkMilk = Q76,
-    EatBreakfast = Q77,
+    NoFruitJuice = Q69,
+    NoFruit = Q70,
+    NoSalad = Q71,
+    NoPotatoes = Q72,
+    NoCarrots = Q73,
+    NoOtherVeggies = Q74,
+    NoSoda = Q75,
+    NoMilk = Q76,
+    NoBreakfast = Q77,
     PhysicalActivity = Q78,
-    HoursTV = Q79,
-    HoursVideoGames = Q80,
-    DaysPEClass = Q81,
+    ThreeOrMoreHoursTV = Q79,
+    ThreeOrMoreHoursVideoGames = Q80,
+    AttendedPEClass = Q81,
     SportsTeam = Q82,
     ConcussionSports = Q83,
     HIVTested = Q84,
     STDTested = Q85,
     DentistVisit = Q86,
     HasAsthma = Q87,
-    HoursSleep = Q88,
-    GradesInSchool = Q89,
-    SportsDrinks = Q92,
-    DrinksWater = Q93,
+    EightorMoreHoursSleep = Q88,
+    MostlyAGradesInSchool = Q89,
+    CurrentPainMedicine = QN90,
+    EverHallucinogenicDrugs = Q91,
+    NoSportsDrinks = Q92,
+    NoDrinksWater = Q93,
     FoodAllergy = Q94,
     MuscleStrengthening = Q95,
     IndoorTanning = Q96,
     UseSunscreen = Q97,
     DifficultyConcentrating = Q98,
     EnglishProficiency = Q99
-  ) |>
-  select(Sex, Race, Age, Grade, SexOrientation, everything())
+  )
 
 usethis::use_data(healthyBehaviors, overwrite = TRUE)
