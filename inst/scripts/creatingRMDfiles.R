2a0adae2b991a3de60d44dfe73733104ac588a81
ccani007 <ccani007@fiu.edu>
2023-10-01 05:34:30 -0400

    Creating YRBS 2017

--- inst/scripts/creatingRMDfiles.R
+++ inst/scripts/creatingRMDfiles.R
@@ -1,9 +1,14 @@
-# Importing the original 2019 and 2021 YRBSS data
+# Importing the original 2017, 2019 and 2021 YRBSS data
 # Catalina Canizares
 # 07-23-2023
 
 
 library(haven)
+
+
+raw2017 <- read_sav("inst/extData/yrbs2017.dat.sav")
+
+usethis::use_data(raw2017, overwrite = TRUE)
 
 raw2019 <- read_sav("inst/extData/yrbs2019.dat.sav")
 
