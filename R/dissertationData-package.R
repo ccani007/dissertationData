f99975b11339805c793d84b30aab10c628ba621e
ccani007 <ccani007@fiu.edu>
2023-12-18 14:44:58 -0500

    Function and new datasets

---
+++ R/dissertationData-package.R
@@ -0,0 +1,13 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom dplyr across
+#' @importFrom dplyr all_of
+#' @importFrom dplyr mutate
+#' @importFrom dplyr select
+#' @importFrom dplyr sym
+#' @importFrom magrittr %>%
+#' @importFrom rlang :=
+## usethis namespace: end
+NULL
