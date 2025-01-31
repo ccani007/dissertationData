1a07a02a844d0a18ec8e51a1983222f8ddd7793e
ccani007 <ccani007@fiu.edu>
2025-01-31 17:28:17 -0500

    raw 2015 YRBS uploaded

---
+++ R/raw2015.R
@@ -0,0 +1,30 @@
+#' Youth Risk Behavior Surveillance System 2015
+#'
+#' @title raw2015
+#'
+#' @docType data
+#'
+#' @usage data(raw2015)
+#'
+#' @description This is the imported raw version of the 2015 YRBS Data.
+#'     This data set monitors priority health risk behaviors that contribute
+#'     markedly to the leading causes of death, disability, and social problems
+#'     among youth and adults in the United States.
+#'
+#'
+#' These broad categories include:
+#' \itemize{
+#'   \item Behaviors that contribute to unintentional injuries and violence
+#'   \item Sexual behaviors that contribute to unintended pregnancy and
+#'     sexually transmitted diseases, including HIV infection
+#'   \item Alcohol and other drug use
+#'   \item Tobacco use
+#'   \item Unhealthy dietary behaviors
+#'   \item Inadequate physical activity
+#' }
+#'
+#'@format A tibble with 15,624 and 239 variables
+#'
+#'
+#'
+"raw2015"
